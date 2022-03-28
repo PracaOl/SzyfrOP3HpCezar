1fd7101902e7b5e14f75d91f23931fccfc838efd
PracaOl <100939454+PracaOl@users.noreply.github.com>
2022-03-28 12:06:49 +0200

    Update Program.cs

--- SzyfrOP3HpCezar/Program.cs
+++ SzyfrOP3HpCezar/Program.cs
@@ -1,28 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Podaj tekst w wielkich literach od A do Z, aby zakodowac je szyfru cezara:");
-string haslo = Console.ReadLine() ?? "";
-Console.WriteLine("Podaj liczbe jako klucz dla szyfru:");
-string liczba = Console.ReadLine() ?? "";
-int l = int.Parse(liczba);
+int l;
+string liczba, haslo;
 
-for (int e = 0; e < haslo.Length; e++)  
+Console.WriteLine("Podaj tekst do zakodowania:");
+haslo = Console.ReadLine() ?? "";
+Console.WriteLine("Podaj liczbe jako klucz:");
+liczba = Console.ReadLine() ?? "";
+l = int.Parse(liczba);
+
+for (int i = 0; i < haslo.Length; i++)
 {
-    int wartosc = (int)haslo[e];
-    Console.WriteLine(wartosc);
-    wartosc =+ l;
-    if ((int)haslo[e] > 90)
+    int wartoscLitery = (int)haslo[i];
+    wartoscLitery = wartoscLitery + l;
+    if (wartoscLitery <= 90)
     {
-    wartosc =- 25;
-    Console.WriteLine(wartosc);
-    } while (wartosc > 90)
-    if ((int)haslo[e] <= 90)
-    {
-        Console.WriteLine(wartosc);
+         
+        char wartoscLiterye = Convert.ToChar(wartoscLitery);
+        string wartosciLiteryeo = Char.ToString(wartoscLiterye);
+        Console.WriteLine(wartosciLiteryeo);
     }
-    if ((int)haslo[e] < 35)
+    else
+    if (wartoscLitery > 90)
     {
-        Console.WriteLine("Blednie zostaly podane informacje.");
-    }
+        wartoscLitery = wartoscLitery - 26;
+        char wartoscLiterye = Convert.ToChar(wartoscLitery);
+        string wartosciLiteryeo = Char.ToString(wartoscLiterye);
+        Console.WriteLine(wartosciLiteryeo);
+    } while (wartoscLitery > 90) ;
 }
